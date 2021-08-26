35aee5f088b8b18ecaaedee1dcccf797ec929b01
Alexandre <a.marquestortoza@gmail.com>
2021-08-26 11:30:39 -0300

    .

--- olaMundo.php
+++ olaMundo.php
@@ -1,8 +1,33 @@
 <?php
 
-    $idadeLista = [12, 23, 50, 14, 34 ,53, 23];
+   
+   $conta1 = [ 
+        'titular' => 'Alex',
+        'saldo ' => 1800
+   ];
+     
 
-    $idadeSelecionada = $idadeLista[3];
-    echo $idadeSelecionada;
+   $conta2 = [
+       'titular' => 'Eduardo',
+       'saldo' => 2000
 
-    count $idadeLista; 
+   ];
+
+   $conta3 = [
+       'titular' => 'Maria',
+       'saldo' => 5000
+   ];
+
+
+   $contaCorrente = [$conta1, $conta2, $conta3];
+
+/*
+for($i = 0; $i < count($contaCorrente);$i++){
+    echo $contaCorrente[$i] ['titular']. PHP_EOL;
+}
+*/
+
+foreach ($contaCorrente as $conta){
+    echo $conta ['titular']. PHP_EOL;
+}
+
